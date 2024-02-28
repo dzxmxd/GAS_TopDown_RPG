5e10a4ddce63442f440bad245ad14373a9e0785e
dzxmxd <dzxmxd@outlook.com>
2024-02-28 23:57:24 +0800

    feat: Add FlowGraph 1.6-5.3 plugin.

---
+++ Plugins/FlowGraph/Source/Flow/Private/FlowModule.cpp
@@ -0,0 +1,17 @@
+// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
+#include "FlowModule.h"
+
+#define LOCTEXT_NAMESPACE "FlowModule"
+
+void FFlowModule::StartupModule()
+{
+}
+
+void FFlowModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FFlowModule, Flow)
