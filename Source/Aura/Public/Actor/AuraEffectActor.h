ec9f6b0b98894c6edcb0c437385148d972503739
dzxmxd <dzxmxd@outlook.com>
2024-02-28 23:56:20 +0800

    feat: Add aura effect actor and test change attribute logic.

---
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -0,0 +1,37 @@
+﻿// Copyright Santa.Wang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraEffectActor();
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnSphereEndOverLap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
