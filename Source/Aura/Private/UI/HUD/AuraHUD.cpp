16e2b34e9667e82f2f89d171d06078ba027bfb4e
dzxmxd <dzxmxd@outlook.com>
2024-03-11 23:31:35 +0800

    feat: Add base aura HUD logic.

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright Santa.Wang
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget =  CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
