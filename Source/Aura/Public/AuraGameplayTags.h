635aa0cce889d77524f51b99f7820155e02a038d
dzxmxd <dzxmxd@outlook.com>
2024-05-16 20:11:17 +0800

    feat: Add AuraGameplayTags to manage all gameplay tags.

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,22 @@
+﻿// Copyright Santa.Wang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; };
+	static void InitializeNativeGameplayTags();
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
