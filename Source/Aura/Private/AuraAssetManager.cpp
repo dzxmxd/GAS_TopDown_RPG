61af6b2a22301570296d9b5ad61b408c1f0fbf88
dzxmxd <dzxmxd@outlook.com>
2024-05-17 16:53:22 +0800

    feat: Add aura asset manager and initialize aura gameplay tags.

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright Santa.Wang
+
+
+#include "AuraAssetManager.h"
+
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FAuraGameplayTags::Get().InitializeNativeGameplayTags();
+}
---
+++ Source/Aura/Public/AuraAssetManager.h
@@ -0,0 +1,22 @@
+﻿// Copyright Santa.Wang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "AuraAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	UAuraAssetManager& Get();
+
+protected:
+	virtual void StartInitialLoading() override;
+};
