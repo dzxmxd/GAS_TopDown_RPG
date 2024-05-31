0b8b0dd5891fda53e12dee4f2ff204903b13d1f2
dzxmxd <dzxmxd@outlook.com>
2024-05-31 18:54:39 +0800

    feat: Add aura projectile spell base class.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Santa.Wang
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	const FString ErrorStr = FString::Printf(TEXT("Projectile spell activate ability called!"));
+	UE_LOG(LogTemp, Error, TEXT("%s:%d %s"), *FString(__FUNCTION__), __LINE__, *ErrorStr);
+	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 10.f, FColor::Red, ErrorStr);
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,19 @@
+﻿// Copyright Santa.Wang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
