b597cecd1f5baaa568e2d09aa16ec16b7e1d3285
dzxmxd <dzxmxd@outlook.com>
2024-08-09 18:22:43 +0800

    feat: Add a custom gameplay effect context.

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright Santa.Wang
+
+#include "AuraAbilityTypes.h"
+
+bool FAuraGameplayEffectContext::IsIsBlockedHit() const
+{
+	return bIsBlockedHit;
+}
+
+void FAuraGameplayEffectContext::SetIsBlockedHit(const bool bInIsBlockedHit)
+{
+	this->bIsBlockedHit = bInIsBlockedHit;
+}
+
+bool FAuraGameplayEffectContext::IsIsCriticalHit() const
+{
+	return bIsCriticalHit;
+}
+
+void FAuraGameplayEffectContext::SetIsCriticalHit(const bool bInIsCriticalHit)
+{
+	this->bIsCriticalHit = bInIsCriticalHit;
+}
+
+UScriptStruct* FAuraGameplayEffectContext::GetScriptStruct() const
+{
+	return FAuraGameplayEffectContext::StaticStruct();
+}
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	
+
+
+	return true;
+}
---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,33 @@
+﻿// Copyright Santa.Wang
+
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+public:
+	bool IsIsBlockedHit() const;
+	void SetIsBlockedHit(const bool bInIsBlockedHit);
+	bool IsIsCriticalHit() const;
+	void SetIsCriticalHit(const bool bInIsCriticalHit);
+	
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const override;
+	
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+private:
+};
