040550957c12996e852a4416d436e71fd136f0b9
dzxmxd <dzxmxd@outlook.com>
2024-08-19 17:18:12 +0800

    feat: Using custom ability system globals class.

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Santa.Wang
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
