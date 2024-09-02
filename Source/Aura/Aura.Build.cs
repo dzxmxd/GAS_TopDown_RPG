c6bd646aa392424f95aef1373b25b45a81971873
dzxmxd <dzxmxd@outlook.com>
2024-09-02 17:25:48 +0800

    feat: Add a behavier tree service to find nearest player.

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -24,7 +24,8 @@ public class Aura : ModuleRules
 			"GameplayTasks",
 			"UMG",
 			"NavigationSystem",
-			"Niagara"
+			"Niagara",
+			"AIModule"
 		});
 
 		// Uncomment if you are using Slate UI
---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright Santa.Wang
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	const APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTags;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTags);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	for (auto* CurActor : ActorsWithTags)
+	{
+		if (IsValid(CurActor) && IsValid(OwningPawn))
+		{
+			const float CurActorDistance = OwningPawn->GetDistanceTo(CurActor);
+			if (CurActorDistance < ClosestDistance)
+			{
+				ClosestDistance = CurActorDistance;
+				ClosestActor = CurActor;
+			}
+		}
+	}
+
+	UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetFollowSelector, ClosestActor);
+	UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTargetSelector, ClosestDistance);
+}
